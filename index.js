88c768ebe3fbbc1f1c12af9b1f3ebd22d2aac975
Sindre Sorhus <sindresorhus@gmail.com>
2018-04-23 20:55:20 +0700

    Improve code readability
    Inspired by and closes #3

--- index.js
+++ index.js
@@ -2,37 +2,50 @@
 const deburr = require('lodash.deburr');
 const escapeStringRegexp = require('escape-string-regexp');
 
-module.exports = (string, opts) => {
-	opts = Object.assign({separator: '-'}, opts);
-
-	if (opts.separator) {
-		opts.separator = escapeStringRegexp(opts.separator);
-	}
-
-	string = deburr(string);
-
-	// Decamelize
-	string = string
+const decamelize = string => {
+	return string
 		.replace(/([a-z\d])([A-Z])/g, `$1 $2`)
 		.replace(/([A-Z]+)([A-Z][a-z\d]+)/g, `$1 $2`);
+};
 
-	// Custom replacements
-	string = string
-		.replace(/&/g, ' and ')
-		.replace(/🦄/g, ' unicorn ')
-		.replace(/♥/g, ' love ');
+const customReplacements = new Map([
+	['&', 'and'],
+	['🦄', 'unicorn'],
+	['♥', 'love']
+]);
 
+const doCustomReplacements = string => {
+	for (const [key, value] of customReplacements) {
+		string = string.replace(new RegExp(escapeStringRegexp(key), 'g'), ` ${value} `);
+	}
+
+	return string;
+};
+
+const removeMootSeparators = (string, separator) => {
+	return string
+		.replace(new RegExp(`${separator}{2,}`, 'g'), separator)
+		.replace(new RegExp(`^${separator}|${separator}$`, 'g'), '');
+};
+
+module.exports = (string, options) => {
+	if (typeof string !== 'string') {
+		throw new TypeError(`Expected a string, got \`${typeof string}\``);
+	}
+
+	options = Object.assign({
+		separator: '-'
+	}, options);
+
+	const separator = escapeStringRegexp(options.separator);
+
+	string = deburr(string);
+	string = decamelize(string);
+	string = doCustomReplacements(string);
 	string = string.toLowerCase();
-
-	string = string.replace(/[^a-z\d]+/g, opts.separator);
-
-	string = string
-		// Remove backslash
-		.replace(new RegExp('\\\\', 'g'), '')
-		// Remove duplicate separators
-		.replace(new RegExp(`${opts.separator}{2,}`, 'g'), opts.separator)
-		// Remove separator from start and end
-		.replace(new RegExp(`^${opts.separator}|${opts.separator}$`, 'g'), '');
+	string = string.replace(/[^a-z\d]+/g, separator);
+	string = string.replace(/\\/g, '');
+	string = removeMootSeparators(string, separator);
 
 	return string;
 };
