d9645ab66774ec3f3b64afe7ef50b4d93c9fb1d7
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-07 22:05:37 +0100

    Add TypeScript definition

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const removeMootSeparators = (string, separator) => {
 		.replace(new RegExp(`^${separator}|${separator}$`, 'g'), '');
 };
 
-module.exports = (string, options) => {
+const slugify = (string, options) => {
 	if (typeof string !== 'string') {
 		throw new TypeError(`Expected a string, got \`${typeof string}\``);
 	}
@@ -64,3 +64,6 @@ module.exports = (string, options) => {
 
 	return string;
 };
+
+module.exports = slugify;
+module.exports.default = slugify;
---
+++ index.test-d.ts
@@ -0,0 +1,10 @@
+import {expectType} from 'tsd-check';
+import {slugify} from '.';
+
+expectType<string>(slugify('I ♥ Dogs'));
+expectType<string>(slugify('BAR and baz', {separator: '_'}));
+expectType<string>(slugify('Déjà Vu!', {lowercase: false}));
+expectType<string>(slugify('fooBar', {decamelize: false}));
+expectType<string>(
+	slugify('I ♥ 🦄 & 🐶', {customReplacements: [['🐶', 'dog']]})
+);
