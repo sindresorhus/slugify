5436039b77acfe68f1c88a67033436e1c9d6eb52
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-06 23:41:40 +0800

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -5,10 +5,8 @@ expectType<string>(slugify('I ♥ Dogs'));
 expectType<string>(slugify('BAR and baz', {separator: '_'}));
 expectType<string>(slugify('Déjà Vu!', {lowercase: false}));
 expectType<string>(slugify('fooBar', {decamelize: false}));
-expectType<string>(
-	slugify('I ♥ 🦄 & 🐶', {customReplacements: [['🐶', 'dog']]})
-	);
-	expectType<string>(slugify('_foo_bar', {preserveLeadingUnderscore: true}));
+expectType<string>(slugify('I ♥ 🦄 & 🐶', {customReplacements: [['🐶', 'dog']]}));
+expectType<string>(slugify('_foo_bar', {preserveLeadingUnderscore: true}));
 
 // counter
 expectType<string>(slugify.counter()('I ♥ Dogs'));
