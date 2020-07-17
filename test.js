462df01084d7a88727538d3fe2d83f0f2f1158d5
Alex <alex190030@gmail.com>
2020-07-17 23:25:54 +0300

    Reverted changes test to master

--- test.js
+++ test.js
@@ -119,7 +119,6 @@ test('supports Romanian', t => {
 });
 
 test('supports Turkish', t => {
-	t.is(slugify('İ ı Ş ş Ç ç Ğ ğ', {lowercase: true, separator: ' '}), 'i i s s c c g g');
 	t.is(slugify('İ ı Ş ş Ç ç Ğ ğ', {lowercase: false, separator: ' '}), 'I i S s C c G g');
 });
 
