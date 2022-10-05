4f73d4fe8f928adc3b1f1b628a0ced90a2f23fc9
Sindre Sorhus <sindresorhus@gmail.com>
2022-10-05 14:52:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const decamelize = string => {
 		.replace(/([a-z\d]+)([A-Z]{2,})/g, '$1 $2')
 
 		.replace(/([a-z\d])([A-Z])/g, '$1 $2')
-		// [a-rt-z] matches all lowercase characters except 's'.
-		// This avoids matching plural acronyms like 'APIs'.
+		// `[a-rt-z]` matches all lowercase characters except `s`.
+		// This avoids matching plural acronyms like `APIs`.
 		.replace(/([A-Z]+)([A-Z][a-rt-z\d]+)/g, '$1 $2');
 };
 
